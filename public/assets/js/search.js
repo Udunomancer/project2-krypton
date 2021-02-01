a1b3efe52c3b0842628ebf1c74a4f09ef78c2595
Kyle Chriscoe <kyle717@gmail.com>
2021-02-01 17:58:55 -0500

    moved card building functionality to function for reuse

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,38 +1,46 @@
-$(document).ready(function() {
-    // Reference to the container
-    const resultsEl = $("#results-contain");
-    // Getting Search Term from the URL
-    const pageURL = window.location.search;
-    const urlParams = new URLSearchParams(pageURL);
-    let searchTerm = urlParams.get('title');
+$(document).ready(function () {
+  // Reference to the container
+  const resultsEl = $("#results-contain");
+  // Getting Search Term from the URL
+  const pageURL = window.location.search;
+  const urlParams = new URLSearchParams(pageURL);
+  let searchTerm = urlParams.get("title");
 
-    searchGameDescriptions();
+  searchGameDescriptions();
 
-    function searchGameDescriptions() {
-        $.ajax({
-            type: "GET",
-            url: "/api/game-description/" + searchTerm
-        }).then((response) => {
-            // <div class="card">
-            // <div class="card-content">
-            console.log(response);
-            for (let i = 0; i < response.length; i++) {
-                let resultCard = $("<div>");
-                resultCard.addClass("card");
-                let resultTitle = $("<div>");
-                resultTitle.addClass("card-title");
-                resultTitle.text(response[i].gameTitle);
-                resultCard.append(resultTitle);
-                let resultContent = $("<div>");
-                resultContent.addClass("card-content");
-                resultContent.text(
-                    "Description: " + response[i].gameDescription + "\n" +
-                    "Players: " + response[i].minPlayers + "-" + 
-                    response[i].maxPlayers
-                );
-                resultCard.append(resultContent);
-                resultsEl.append(resultCard);
-            }
-        })
-    }
-})
+  function searchGameDescriptions() {
+    $.ajax({
+      type: "GET",
+      url: "/api/game-description/" + searchTerm,
+    }).then((response) => {
+      // <div class="card">
+      // <div class="card-content">
+      console.log(response);
+      for (let i = 0; i < response.length; i++) {
+          buildResultCard(response[i]);
+      }
+    });
+  }
+
+  function buildResultCard(currentResult) {
+    let resultCard = $("<div>");
+    resultCard.addClass("card");
+    let resultTitle = $("<div>");
+    resultTitle.addClass("card-title");
+    resultTitle.text(currentResult.gameTitle);
+    resultCard.append(resultTitle);
+    let resultContent = $("<div>");
+    resultContent.addClass("card-content");
+    resultContent.text(
+      "Description: " +
+        currentResult.gameDescription +
+        "\n" +
+        "Players: " +
+        currentResult.minPlayers +
+        "-" +
+        currentResult.maxPlayers
+    );
+    resultCard.append(resultContent);
+    resultsEl.append(resultCard);
+  }
+});
