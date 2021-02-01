6c40991ec6c9db29b9884bd1827e1b04b9608043
Kyle Chriscoe <kyle717@gmail.com>
2021-02-01 17:54:07 -0500

    added results card to search results page

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // Reference to the container
-    const containEl = $("#contain");
+    const resultsEl = $("#results-contain");
     // Getting Search Term from the URL
     const pageURL = window.location.search;
     const urlParams = new URLSearchParams(pageURL);
@@ -13,7 +13,26 @@ $(document).ready(function() {
             type: "GET",
             url: "/api/game-description/" + searchTerm
         }).then((response) => {
+            // <div class="card">
+            // <div class="card-content">
             console.log(response);
+            for (let i = 0; i < response.length; i++) {
+                let resultCard = $("<div>");
+                resultCard.addClass("card");
+                let resultTitle = $("<div>");
+                resultTitle.addClass("card-title");
+                resultTitle.text(response[i].gameTitle);
+                resultCard.append(resultTitle);
+                let resultContent = $("<div>");
+                resultContent.addClass("card-content");
+                resultContent.text(
+                    "Description: " + response[i].gameDescription + "\n" +
+                    "Players: " + response[i].minPlayers + "-" + 
+                    response[i].maxPlayers
+                );
+                resultCard.append(resultContent);
+                resultsEl.append(resultCard);
+            }
         })
     }
 })
