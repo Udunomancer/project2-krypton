71801be905b312279cc405716ab802b0277ce14b
mugi <munkhuu1233@gmail.com>
2021-02-01 22:51:52 -0500

    reformated

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // Route that will return and display home page (index)
 router.get("/", (req, res) => {
   res.render("index");
-})
+});
 
 // Route that will return and display new user form
 router.get("/signup", (req, res) => {
@@ -21,16 +21,16 @@ router.get("/login", (req, res) => {
 
 // Route to create a new user
 // User entered details from /signup sent to database
-router.post("/api/signup", function(req, res) {
+router.post("/api/signup", function (req, res) {
   db.User.create({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: req.body.password,
   })
-    .then(function() {
-     res.status(200).end();
+    .then(function () {
+      res.status(200).end();
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.status(401).json(err);
     });
 });
@@ -51,87 +51,73 @@ router.post("/api/game-description/new", (req, res) => {
   //     console.log(err);
   //     res.status(500).end();
   //   }))
-  db.GameDescription.create({
-    gameTitle: req.body.gameTitle,
-    playerAge: req.body.playerAge,
-    published: req.body.published,
-    minPlayers: req.body.minPlayers,
-    maxPlayers: req.body.maxPlayers,
-    minPlayTime: req.body.minPlayTime,
-    maxPlayTime: req.body.maxPlayTime,
-    gameDescription: req.body.gameDescription
-  }, {
-    fields: [
-      "gameTitle", "playerAge", "published", "minPlayers", "maxPlayers",
-      "minPlayTime", "maxPlayTime", "gameDescription"
-    ]
-  })
-})
+  db.GameDescription.create(
+    {
+      gameTitle: req.body.gameTitle,
+      playerAge: req.body.playerAge,
+      published: req.body.published,
+      minPlayers: req.body.minPlayers,
+      maxPlayers: req.body.maxPlayers,
+      minPlayTime: req.body.minPlayTime,
+      maxPlayTime: req.body.maxPlayTime,
+      gameDescription: req.body.gameDescription,
+    },
+    {
+      fields: [
+        "gameTitle",
+        "playerAge",
+        "published",
+        "minPlayers",
+        "maxPlayers",
+        "minPlayTime",
+        "maxPlayTime",
+        "gameDescription",
+      ],
+    }
+  );
+});
 // === API Routes ===
 // Route to render all trains to a page
 router.get("/games", (req, res) => {
+  db.User.findAll({}).then(function (data) {
+    db.GameUnit.findAll({
+      include: [db.User],
+    })
+      .then(function (games) {
+        res.render("all-games", { gameunits: games, users: data });
+        // res.json(games);
+        console.log(games);
+      })
+      .catch((err) => {
+        console.log(err);
 
-  db.User.findAll({}).then(function(data){
-
-
- db.GameUnit.findAll({
-  include: [db.User]
-}).then(function(games) {
-
-  res.render("all-games", { gameunits: games, users: data });
-  // res.json(games);
-  console.log(games);
-})
-.catch((err) => {
-      console.log(err);
-    
-      res.status(500).end();
-    });
-
+        res.status(500).end();
+      });
   });
- 
 });
 
 router.get("/games/:userId", (req, res) => {
-
-  
-  db.User.findAll({}).then(function(data){
-
+  db.User.findAll({}).then(function (data) {
     var query = {};
     if (req.params.userId) {
-  query.UserId = req.params.userId;
-};
-  
- db.GameUnit.findAll({
-  where: query,
-  include: [db.User]
-}).then(function(games) {
+      query.UserId = req.params.userId;
+    }
 
-  res.render("all-games", { gameunits: games, users: data });
-  // res.json(games);
-  console.log(games);
-})
-.catch((err) => {
-      console.log(err);
-    
-      res.status(500).end();
-    });
+    db.GameUnit.findAll({
+      where: query,
+      include: [db.User],
+    })
+      .then(function (games) {
+        res.render("all-games", { gameunits: games, users: data });
+        // res.json(games);
+        console.log(games);
+      })
+      .catch((err) => {
+        console.log(err);
 
+        res.status(500).end();
+      });
   });
-
-  
-
-  });
- 
-
-
-
-
-
-
-
-
-
-
+});
 
 module.exports = router;
